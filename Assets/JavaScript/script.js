a965124f07509a3d43ba54114ba6772150c6f07b
Richard Yang <richard.yang.tong@gmail.com>
2021-01-20 18:43:04 -0500

    Added lower case letter characters to the collection object.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 const characterCollection = {
-  number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 }
 
 // Write password to the #password input
