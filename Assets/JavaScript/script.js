3b0cd77dc93df852a7bf53248104a4f015591b48
Richard Yang <richard.yang.tong@gmail.com>
2021-01-21 13:23:39 -0500

    Added comments to improve readability of script.js.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,6 +1,7 @@
-// Assignment Code
+// Refer to the generate button in index.html.
 var generateBtn = document.querySelector("#generate");
 
+// Define the character set that will be used to generate the password.
 const characterCollection = [
   ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
   ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
@@ -8,6 +9,7 @@ const characterCollection = [
   ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
 ]
 
+// Helper function: typeSelector(), takes 4 user inputs from window.comfirm and return a list of 4 booleans.
 function typeSelector ()  {
   var result = [];
   result[0] = window.confirm("Include numbers in your password? \nIf you do, click 'Ok', otherwise click 'Cancel' to move on.");
@@ -17,6 +19,8 @@ function typeSelector ()  {
   return result;
 }
 
+// Helper function: makeSample(), takes a list of 4 booleans and return a list of strings.
+// Note: this function was designed to used to process the output from typeSelector(), if the length of input list is greater than 4, it might run into problems.
 function makeSample(inputs) {
   var sample = [];
   for (let i = 0; i < inputs.length; i++) {
@@ -27,6 +31,7 @@ function makeSample(inputs) {
   return sample;
 }
 
+// Helper function: generatePassword(), takes a list of strings and a number, return a single string.
 function generatePassword (sample, length) {
   let password = ""
   for (let i = 0; i < length; i++) {
@@ -38,14 +43,15 @@ function generatePassword (sample, length) {
 
 // Write password to the #password input
 function writePassword() {
-
   // Ask user to enter the desire length of password, if requirement is not met, ask user to re-enter the length.
   let userLength = parseInt(window.prompt("Please enter the desired length of your password, between 8 and 128."));
   let userType = typeSelector();
 
+  // Check if the input from user meets the criterias.
   let lengthChecker = userLength >=8 && userLength <= 128;
   let typeChecker = userType.includes(true);
 
+  // For invalid input, print an error message and ask the user to re-enter or exit.
   if (!lengthChecker && !typeChecker) {
     let userResponse = window.confirm("Invalid length: must be between 8 and 128. \nInvalid type: must include at least one type of character. \nClick 'Ok' to try again or click 'Cancel' to exit.");
     if (userResponse) {
@@ -68,6 +74,7 @@ function writePassword() {
       return;
     }
   } else {
+    // If all inputs are valid, produce the password.
     let userSample = makeSample(userType);
     let password = generatePassword(userSample, userLength);
     let passwordText = document.querySelector("#password");
