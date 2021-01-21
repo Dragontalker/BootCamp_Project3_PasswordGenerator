2745c2b8b0c38709b2fc01afea9cd567cc2fdeab
Richard Yang <richard.yang.tong@gmail.com>
2021-01-21 11:01:32 -0500

    First attempt to put javascript and html together.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -8,7 +8,7 @@ const characterCollection = [
   ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
 ]
 
-function typeSelector()  {
+function typeSelector ()  {
   var result = [];
   result[0] = window.confirm("Do you want to include numbers in your password?");
   result[1] = window.confirm("Do you want to include lower case letters in your passowrd?");
@@ -28,13 +28,78 @@ function makeSample(inputs) {
 }
 
 
+function grabInput () {
+  var userLength;
+  userLength = parseInt(window.prompt("Please enter the desired length of your password, between 8 and 128."));
+  if (!userLength) {
+    let userResponse = window.confirm("No length was received, click 'Yes' to re-enter the desired length of your password, click 'No' to exit.");
+    if (userResponse) {
+      grabInput();
+    } else {
+      return;
+    }
+  } else {
+    if (userLength >= 8 && userLength <= 128) {
+      return userLength;
+    } else {
+      let userResponse = window.confirm("The length of your password must be between 8 and 128, click 'Yes' to re-enter the desired length of your password, click 'No' to exit.");
+      if (userResponse) {
+        grabInput();
+      } else {
+        return;
+      }
+    } 
+  } 
+}
+
+function generatePassword (sample, length) {
+  let password = ""
+  for (let i = 0; i < length; i++) {
+    whichIndex = Math.floor(Math.random() * sample.length);
+    password = password.concat(sample[whichIndex]);
+  }
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var userLength;
+  var userInput;
+  var userSample;
+  var password;
 
+  // Ask user to enter the desire length of password, if requirement is not met, ask user to re-enter the length.
+  userLength = parseInt(window.prompt("Please enter the desired length of your password, between 8 and 128."));
+  if (!userLength) {
+    let userResponse = window.confirm("No length was received, click 'Yes' to re-enter the desired length of your password, click 'No' to exit.");
+    if (userResponse) {
+      writePassword();
+    } else {
+      return;
+    }
+  } else {
+    if (userLength >= 8 && userLength <= 128) {
+      return userLength;
+    } else {
+      let userResponse = window.confirm("The length of your password must be between 8 and 128, click 'Yes' to re-enter the desired length of your password, click 'No' to exit.");
+      if (userResponse) {
+        writePassword();
+      } else {
+        return;
+      }
+    } 
+  } 
+
+  // Ask the user to select least one type of characters to be included in the password, if none selected, ask for re-enter.
+  userInput = typeSelector();
+  
+  // When all requirements are met, producing the password.
+  userSample = makeSample(userInput);
+  password = generatePassword(userSample, userLength);
+  
+  var passwordText;
+  passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
