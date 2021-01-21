e35992e466105905f89a266d20b29a81877431b6
Richard Yang <richard.yang.tong@gmail.com>
2021-01-20 19:21:03 -0500

    Added a function to assemble the sample to random select from based on user input.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const characterCollection = {
-  number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  special: ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
-}
+const characterCollection = [
+  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
+]
 
 function typeSelector()  {
   var result = [];
@@ -15,6 +15,16 @@ function typeSelector()  {
   result[2] = window.confirm("Do you want to include upper case letters in your password?");
   result[3] = window.confirm("Do you want to include special characters in your password?");
   return result;
+}
+
+function makeSample(inputs) {
+  var sample = [];
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i]) {
+      sample = sample.concat(characterCollection[i]);
+    }
+  }
+  return sample;
 }
 
 
