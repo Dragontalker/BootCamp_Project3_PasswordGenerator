8c8a6344fe735ebb11498398488eab60f22cbfb1
Richard Yang <richard.yang.tong@gmail.com>
2021-01-20 19:00:04 -0500

    Added a function to collect user input for what types of characters should be included in his/her password.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -5,8 +5,18 @@ const characterCollection = {
   number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
   lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  special: ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[','{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
+  special: ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', ',', '<', '.', '>', '/', '?']
 }
+
+function typeSelector()  {
+  var result = [];
+  result[0] = window.confirm("Do you want to include numbers in your password?");
+  result[1] = window.confirm("Do you want to include lower case letters in your passowrd?");
+  result[2] = window.confirm("Do you want to include upper case letters in your password?");
+  result[3] = window.confirm("Do you want to include special characters in your password?");
+  return result;
+}
+
 
 // Write password to the #password input
 function writePassword() {
